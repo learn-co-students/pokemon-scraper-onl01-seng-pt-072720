d0edd8867c5cce2e1606f570f3112a483c5bc78f
yani82 <yani.iman1@gmail.com>
2020-10-08 02:05:25 -0400

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,3 +1,10 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+-- sqlite3 path/to/database.db < source_of_sql_data
+
+-- CREATE TABLE pokemon (
+--     id integer primary key autoincrement,
+--     name text,
+--     type text
+-- ); 
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,42 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:nil, name:, type:, db:)
+        @id = id
+        @name = name 
+        @type = type 
+        @db = db
+    end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type) 
+        VALUES (?, ?);
+        SQL
+
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end 
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon WHERE id = (?);
+        SQL
+
+        db.execute(sql, id).map do |pokemon|
+            id = pokemon[0]
+            name = pokemon[1]
+            type = pokemon[2]
+        Pokemon.new(id: id, name: name, type: type, db: db)
+        end.first
+    end 
+
+    # def alter_hp(new_hp)
+    #     sql = <<-SQL
+    #     UPDATE pokemon SET hp = ? WHERE id = ?;
+    #     SQL
+    #     db.execute(sql, [new_hp, id])
+    # end 
+    
 end
